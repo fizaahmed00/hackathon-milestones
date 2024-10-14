5599d7413383d6bb5460f292a07c8ee08daf3ab3
fizaahmed00 <fizaahmed572@gmail.com>
2024-10-14 19:33:47 +0500

    Add files via upload

---
+++ milestone 1/script.js
@@ -0,0 +1,10 @@
+var toggleButton = document.getElementById('toggle-skills');
+var Skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (Skills.style.display === 'none') {
+        Skills.style.display = 'block';
+    }
+    else {
+        Skills.style.display = 'none';
+    }
+});
---
+++ milestone 3/main.js
@@ -0,0 +1,28 @@
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //prevaent page reload
+    //collect values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //generate resume content dynamically
+    var resumeHTML = "\n    <h2><b>Resume<b/></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n    \n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n");
+    //display genreted resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume display element is missing');
+    }
+    // if(resumeOutputElement){
+    //     resumeDisplayElement.innerHTML= resumeHTML;
+    // }
+    // else{
+    //     console.error('The resume display element is missing')
+    // }
+});
+
